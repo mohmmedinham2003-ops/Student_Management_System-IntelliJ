7ace087e89f364e7e3ae9e338cc0cc8d21631878
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-09-20 10:49:36 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/Login_page.fxml"))));
+        stage.show();
+    }
+}
+
