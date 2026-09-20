d8a2ceb62d39cc7562badb07e89e77e4852bbcb6
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-09-20 15:09:18 +0530

    creating a button

--- src/main/java/LoginPageController.java
+++ src/main/java/LoginPageController.java
@@ -1,8 +1,42 @@
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class LoginPageController {
 
-    public void btnSubmitOnAction(ActionEvent actionEvent) {
-        System.out.println("Button clicked!!!");
+    @FXML
+    private Button btn01;
+
+    @FXML
+    private Button btn02;
+
+    @FXML
+    private Button btn03;
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    void btn01OnAction(ActionEvent event) {
+        System.out.println("btn1 clicked");
     }
+
+    @FXML
+    void btn02OnAction(ActionEvent event) {
+        System.out.println("btn2 clicked");
+
+    }
+
+    @FXML
+    void btn03OnAction(ActionEvent event) {
+        System.out.println("btn3 clicked");
+
+    }
+
+    @FXML
+    void btnSubmitOnAction(ActionEvent event) {
+        System.out.println("btn clicked");
+
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,5 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
+public class Main {
     public static void main(String[] args) {
-        launch();
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/login_page.fxml"))));
-        stage.show();
+        Starter.main(args);
     }
 }
-
