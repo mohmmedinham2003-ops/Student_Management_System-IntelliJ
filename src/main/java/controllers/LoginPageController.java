256a0f85ef50e33019a335ca7d32377a93506063
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-09-26 16:49:10 +0530

    refactor(login): remove placeholder buttons and add login page UI

--- src/main/java/controllers/LoginPageController.java
+++ src/main/java/controllers/LoginPageController.java
@@ -6,38 +6,4 @@ import javafx.scene.control.Button;
 
 public class LoginPageController {
 
-    @FXML
-    private Button btn01;
-
-    @FXML
-    private Button btn02;
-
-    @FXML
-    private Button btn03;
-
-    @FXML
-    private Button btnSubmit;
-
-    @FXML
-    void btn01OnAction(ActionEvent event) {
-        System.out.println("btn1 clicked");
-    }
-
-    @FXML
-    void btn02OnAction(ActionEvent event) {
-        System.out.println("btn2 clicked");
-
-    }
-
-    @FXML
-    void btn03OnAction(ActionEvent event) {
-        System.out.println("btn3 clicked");
-
-    }
-
-    @FXML
-    void btnSubmitOnAction(ActionEvent event) {
-        System.out.println("btn clicked");
-    }
-
 }
